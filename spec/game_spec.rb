141676cc3c4f17dbdad841239a5ee9481481ef26
jfhinchcliffe <hinchy@gmail.com>
2016-09-27 15:07:40 +1000

    Added some testing for the team class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,9 +4,17 @@ require './team'
 describe Game do
 
     before do
+      Game.finish_season
       @team1 = Team.new({name: "Bulls", attack: 9, defense: 5, luck: 3, condition_preference: "dry"})
       @team2 = Team.new({name: "SnuggleMonsters", attack: 9, defense: 5, luck: 3, condition_preference: "dry"})
       @game = Game.new(@team1, @team2)
+    end
+
+    describe "game information" do
+      it "outputs the game information home team, away team" do
+        expect(@game.home_team.name).to eql(@team1.name)
+        expect(@game.away_team.name).to eql(@team2.name)
+      end
     end
 
     describe "check" do
@@ -31,5 +39,13 @@ describe Game do
       end
 
     end
+
+    # describe "saving" do
+    #
+    #   it "outputs the contents of the seasons file" do
+    #     expect(Game.season_results).to output("{@game.result[:home_team]} vs #{@game.result[:away_team]}, score #{@game.result[:score]}").to_stdout
+    #   end
+    # end
+    #{@game.result[:home_team]} vs #{@game.result[:away_team]}, score #{@game.result[:score]}"
 
 end
---
+++ spec/team_spec.rb
@@ -0,0 +1,28 @@
+require './team'
+
+describe Team do
+
+  before do
+    @team = Team.new({name: "Bulls", attack: 9, defense: 5, luck: 3, condition_preference: "dry"})
+  end
+
+  describe "team information" do
+    it "outputs the team information" do
+      expect(@team.name).to eql("Bulls")
+      expect(@team.attack).to eql(9)
+      expect(@team.defense).to eql(5)
+      expect(@team.luck).to eql(3)
+      expect(@team.condition_preference).to eql('dry')
+    end
+  end
+
+  describe "edit" do
+    it "edits team values" do
+      @team.edit_team_values("Snakes", 'wet')
+      expect(@team.name).to eql("Snakes")
+      expect(@team.luck).to eql(3)
+      expect(@team.condition_preference).to eql('wet')
+    end
+  end
+
+end
